88223348691e1a76dec55c26409bcb3f137770e2
mathias <mathias@beirer.com>
2022-02-02 21:41:25 +0100

    .

--- Tests/2_klasse_Test1/script.js
+++ Tests/2_klasse_Test1/script.js
@@ -1,3 +1,10 @@
 document.getElementById("button").addEventListener("click", function() {
     generate();
 })
+
+function giveResult() {
+    let number = document.getElementById("textfield").value;
+
+    let rounded = Math.round(number);
+    console.log(rounded);
+}
