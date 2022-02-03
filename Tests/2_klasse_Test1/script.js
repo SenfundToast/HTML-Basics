e8aca58bd35433348dc188b42d46019d94399657
mathias <mathias@beirer.com>
2022-02-03 16:39:40 +0100

    Test

--- Tests/2_klasse_Test1/script.js
+++ Tests/2_klasse_Test1/script.js
@@ -1,10 +1,27 @@
 document.getElementById("button").addEventListener("click", function() {
-    generate();
-})
+    giveResult();
+});
 
 function giveResult() {
     let number = document.getElementById("textfield").value;
 
     let rounded = Math.round(number);
     console.log(rounded);
+
+    if (rounded > 0) {
+        let output = "";
+        for (index = 0; index <= rounded; index++) {
+
+            if (index < rounded) {
+                output += index + ", ";
+            } else {
+                output += index;
+            }
+
+            document.getElementById("result").innerHTML = output;
+        }
+
+    } else {
+        document.getElementById("result").innerHTML = 0;
+    }
 }
